ea42a7dc4b2ae1f30bab3d3dfadfb0e11a0e97d6
Richard Käll <richard.kall@me.com>
2016-08-14 00:45:31 +0200

    Add Apollo Server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,31 @@
+import bodyParser from 'body-parser';
 import compression from 'compression';
 import express from 'express';
 import morgan from 'morgan';
 
+import {
+  apolloExpress,
+  graphiqlExpress
+} from 'apollo-server';
+
 import config from '../config';
+import schema from './data/schema';
 
 const DEBUG = config.env !== 'production';
 const server = express();
 
 server.use(compression());
 server.use(morgan(DEBUG ? 'dev' : 'combined'));
+
+server.use('/graphql', bodyParser.json(), apolloExpress({
+  schema
+}));
+
+if (DEBUG) {
+  server.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql'
+  }));
+}
 
 server.listen(config.server.port, () =>
   console.info(`Server running in ${server.get('env')} on port ${config.server.port}`) // eslint-disable-line no-console
